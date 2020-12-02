c22a2250b0b1ae917bc7a6d2892b768f00955ba0
ja201 <50411778+ja201@users.noreply.github.com>
2020-12-02 21:32:59 +0900

    인트로

---
+++ SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.buspick;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },1200);
+
+    }
+
+}
